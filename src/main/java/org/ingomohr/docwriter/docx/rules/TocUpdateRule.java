27a442eebdae4f53835c6fc31b579449cc347354
Ingo Mohr <tellastory73@gmail.com>
2020-05-23 01:37:56 +0200

    fix: TocUpdateRule shall pass if there's not ToC

--- src/main/java/org/ingomohr/docwriter/docx/rules/TocUpdateRule.java
+++ src/main/java/org/ingomohr/docwriter/docx/rules/TocUpdateRule.java
@@ -1,13 +1,19 @@
 package org.ingomohr.docwriter.docx.rules;
 
+import org.docx4j.TraversalUtil;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
 import org.docx4j.toc.TocException;
+import org.docx4j.toc.TocFinder;
 import org.docx4j.toc.TocGenerator;
+import org.docx4j.wml.Body;
+import org.docx4j.wml.Document;
+import org.docx4j.wml.SdtBlock;
 
 /**
  * Rule to update an existing table-of-contents (ToC).
  * <p>
- * If there is no ToC in the document, this rule will not execute successfully.
+ * If there is no ToC in the document, this rule does nothing.
  * </p>
  * 
  * @since 1.3
@@ -23,14 +29,24 @@ public class TocUpdateRule implements DocumentRule {
 	public void apply(Object pObject) {
 
 		WordprocessingMLPackage doc = (WordprocessingMLPackage) pObject;
+		MainDocumentPart documentPart = doc.getMainDocumentPart();
+		Document wmlDocumentEl = (Document) documentPart.getJaxbElement();
+		Body body = wmlDocumentEl.getBody();
 
-		boolean skipPageNumbering = isSkippingPageNumbering();
+		TocFinder finder = new TocFinder();
+		new TraversalUtil(body.getContent(), finder);
 
-		try {
-			TocGenerator generator = new TocGenerator(doc);
-			generator.updateToc(skipPageNumbering);
-		} catch (TocException e) {
-			throw new RuntimeException("Cannot update table of contents", e);
+		SdtBlock toc = finder.getTocSDT();
+
+		if (toc != null) {
+			boolean skipPageNumbering = isSkippingPageNumbering();
+
+			try {
+				TocGenerator generator = new TocGenerator(doc);
+				generator.updateToc(skipPageNumbering);
+			} catch (TocException e) {
+				throw new RuntimeException("Cannot update table of contents", e);
+			}
 		}
 
 	}
